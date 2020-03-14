c7694886e794d82788d71904a068df29c71ca6ee
maks <temnepatypa17@gmail.com>
2020-03-14 12:32:23 +0200

    add bus simulator

---
+++ fake_bus.py
@@ -0,0 +1,44 @@
+from sys import stderr
+import json
+import os
+
+import trio
+from trio_websocket import open_websocket_url
+
+BUS_DATA_PATH = 'routes'
+
+
+def load_routes(directory_path=BUS_DATA_PATH):
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".json"):
+            filepath = os.path.join(directory_path, filename)
+            with open(filepath, 'r', encoding='utf8') as file:
+                yield json.load(file)
+
+
+async def send_bus_coordinates(route):
+    route_name = route["name"]
+    # start_station = route["station_start_name"]
+    # end_station = route["station_stop_name"]
+    # route_stations = route["stations"]
+    coordinates = (x for x in route["coordinates"])
+    try:
+        async with open_websocket_url('ws://127.0.0.1:8080') as ws:
+            for lat, lng in coordinates:
+                await ws.send_message(json.dumps(
+                    {"busId": route_name, "lat": lat, "lng": lng, "route": route_name}, ensure_ascii=False)
+                )
+                return
+    except OSError as ose:
+        print('Connection attempt failed: %s' % ose, file=stderr)
+
+
+async def main():
+    async with trio.open_nursery() as nursery:
+        for route in load_routes():
+            nursery.start_soon(send_bus_coordinates, route)
+
+
+if __name__ == '__main__':
+    trio.run(main)
+
--- server.py
+++ server.py
@@ -1,20 +1,61 @@
+import json
+from functools import partial
+
 import trio
 from trio_websocket import serve_websocket, ConnectionClosed
 
 
-async def echo_server(request):
+data = {
+  "msgType": "Buses",
+  "buses": [
+    {"busId": "c790сс", "lat": 55.7500, "lng": 37.600, "route": "120"},
+    {"busId": "a134aa", "lat": 55.7494, "lng": 37.621, "route": "670к"},
+  ]
+}
+with open("busRoute.json", "r") as f:
+    DATA = json.loads(f.read())
+
+coordinates = (x for x in DATA["coordinates"])
+
+
+async def listen_bus_route_data(request):
+    """
+    Get data about buses route from web socket
+    """
+    sock = await request.accept()
+    while True:
+        try:
+            bus_route = await sock.get_message()
+            print(bus_route)
+        except ConnectionClosed:
+            break
+
+
+async def listen_browser(request):
     ws = await request.accept()
     while True:
         try:
-            message = await ws.get_message()
-            print(message)
-            await ws.send_message(message)
+            #message = await ws.get_message()
+            for lat, lng in coordinates:
+                data = json.dumps({
+                    "msgType": "Buses",
+                    "buses": [
+                        {"busId": "156-0", "lat": lat, "lng": lng, "route": DATA["name"]},
+                    ]
+                })
+                await ws.send_message(data)
+                await trio.sleep(1)
+            raise Exception
         except ConnectionClosed:
             break
 
 
 async def main():
-    await serve_websocket(echo_server, "127.0.0.1", 8000, ssl_context=None)
+    open_data_socket = partial(serve_websocket, listen_bus_route_data, "127.0.0.1", 8080, ssl_context=None)
+    #open_browser_socket = partial(serve_websocket, listen_browser, "127.0.0.1", 8000, ssl_context=None)
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(open_data_socket)
+        # nursery.start_soon(open_browser_socket)
 
 
 if __name__ == "__main__":
